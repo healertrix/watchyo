1f042eb77a86c4a537c96a81ab3df407bf3d1729
Abhinav Kumar <abhinav.webdj@gmail.com>
2024-09-05 17:35:08 +0530

    sizing of the moon icon changed to bigger

--- components/ModeToggle.tsx
+++ components/ModeToggle.tsx
@@ -16,20 +16,16 @@ export function ModeToggle() {
   const { setTheme } = useTheme();
 
   return (
-    <div className='p-1 sm:p-1.5 w-full text-right'>
+    <div className="p-1 sm:p-1.5 w-full text-right">
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
-          <Button
-            variant='outline'
-            size='icon'
-            className='h-8 w-8 sm:h-9 sm:w-9'
-          >
-            <Sun className='h-[1rem] w-[1rem] sm:h-[1.2rem] sm:w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
-            <Moon className='absolute h-[1rem] w-[1rem] sm:h-[1.2rem] sm:w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
-            <span className='sr-only'>Toggle theme</span>
+          <Button variant="outline" size="icon" className="h-8 w-8 sm:h-9 sm:w-9">
+            <Sun className="h-[1rem] w-[1rem] sm:h-[1.2rem] sm:w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+            <Moon className="absolute h-[1rem] w-[1rem] sm:h-[1.2rem] sm:w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+            <span className="sr-only">Toggle theme</span>
           </Button>
         </DropdownMenuTrigger>
-        <DropdownMenuContent align='end'>
+        <DropdownMenuContent align="end">
           <DropdownMenuItem onClick={() => setTheme('light')}>
             Light
           </DropdownMenuItem>
