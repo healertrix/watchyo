a6597b237decd94c5977df5c8117fae495acdfd2
Abhinav Kumar <abhinav.webdj@gmail.com>
2024-09-05 17:33:14 +0530

    sizing of the moon icon changed and the Resturant card refactored

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -12,11 +12,28 @@ export default function HomePage() {
   // );
   // mcd.display();
   // console.log('Hello from the home page!');
-
+const mcd = new Restaurant(
+  "McDonald's",
+  'Fast Food',
+  3.5,
+  '123-456-7890',
+  'https://www.mcdonalds.com',
+  '123 Main St, Anytown, USA',
+  'https://lh3.googleusercontent.com/p/AF1QipO4YCV1zTXer5FOi7iUS0a8YtPUoyAWoObJ3ppC=s1360-w1360-h1020'
+);
+const kfc = new Restaurant(
+  'KFC',
+  'Fast Food',
+  4.5,
+  '123-456-7890',
+  'https://www.kfc.com',
+  '45 Main St, Minneapolis, Canada',
+  'https://lh5.googleusercontent.com/p/AF1QipPxU38eYIJtE556UcRzqQpU0MBRuoHY9ywGa_Lh=w1360-h1020'
+);
   return (
     <div>
-      <ResturantCard></ResturantCard>
-      <ResturantCard></ResturantCard>
+      <ResturantCard restaurant={mcd}></ResturantCard>
+      <ResturantCard restaurant={kfc}></ResturantCard>
     </div>
   );
 }
--- components/mycomponents/ResturantCard.tsx
+++ components/mycomponents/ResturantCard.tsx
@@ -16,7 +16,6 @@ import Link from 'next/link';
 import { MoveUp, MoveDown, ExternalLink } from 'lucide-react';
 import { Link as LucideLink } from 'lucide-react';
 
-export default function ResturantCard() {
   const mcd = new Restaurant(
     "McDonald's",
     'Fast Food',
@@ -24,45 +23,56 @@ export default function ResturantCard() {
     '123-456-7890',
     'https://www.mcdonalds.com',
     '123 Main St, Anytown, USA',
-    'https://www.nrn.com/sites/nrn.com/files/styles/article_featured_retina/public/Mcd%20Drive%20Thru.jpg?itok=DeF7fayX'
+    'https://lh5.googleusercontent.com/p/AF1QipPxU38eYIJtE556UcRzqQpU0MBRuoHY9ywGa_Lh=w1360-h1020'
   );
+  const kfc = new Restaurant(
+    'KFC',
+    'Fast Food',
+    4.5,
+    '123-456-7890',
+    'https://www.kfc.com',
+    '123 Main St, Anytown, USA',
+    'https://lh5.googleusercontent.com/p/AF1QipPxU38eYIJtE556UcRzqQpU0MBRuoHY9ywGa_Lh=w1360-h1020'
+  );
+
+export default function RestaurantCard({ restaurant }: { restaurant: Restaurant }) {
   return (
-    <Card className='flex flex-col md:flex-row h-auto m-4'>
-      <div className='flex flex-col w-full md:w-3/5'>
-        <CardHeader className='p-4'>
-          <CardTitle>{mcd.name}</CardTitle>
-          <div className='flex flex-row'>
-            <CardDescription>{mcd.category}</CardDescription>
-            <Link href={mcd.website} className='mx-3' target='_blank'>
-              <ExternalLink className='h-4 w-4' />
+    <Card className="flex flex-col md:flex-row h-auto m-4">
+      <div className="flex flex-col w-full md:w-3/5">
+        <CardHeader className="p-4">
+          <CardTitle>{restaurant.name}</CardTitle>
+          <div className="flex flex-row">
+            <CardDescription>{restaurant.category}</CardDescription>
+            <Link href={restaurant.website} className="mx-3" target="_blank">
+              <ExternalLink className="h-4 w-4" />
             </Link>
           </div>
         </CardHeader>
-        <CardContent className='p-4 flex-grow'>
-          <p>Address: {mcd.fullAddress}</p>
-          <p>Phone: {mcd.phoneNumber}</p>
-          <p>Rating: {mcd.rating}</p>
+        <CardContent className="p-4 flex-grow">
+          <p>Address: {restaurant.fullAddress}</p>
+          <p>Phone: {restaurant.phoneNumber}</p>
+          <p>Rating: {restaurant.rating}</p>
         </CardContent>
-        <CardFooter className='p-4'>
-          <Button variant='outline' size='icon'>
-            <MoveUp className='h-4 w-4' color='green' />
+        <CardFooter className="p-4">
+          <Button variant="outline" size="icon">
+            <MoveUp className="h-4 w-4" color="green" />
           </Button>
-          <Button variant='outline' size='icon' className='mx-4'>
-            <MoveDown className='h-4 w-4' color='red' />
+          <Button variant="outline" size="icon" className="mx-4">
+            <MoveDown className="h-4 w-4" color="red" />
           </Button>
         </CardFooter>
       </div>
-
-      <div className='w-full h-32 md:w-2/5 md:h-auto relative'>
+      
+      <div className="w-full h-32 md:w-2/5 md:h-auto relative">
         <Image
-          src='https://lh5.googleusercontent.com/p/AF1QipPxU38eYIJtE556UcRzqQpU0MBRuoHY9ywGa_Lh=w1360-h1020'
-          alt='McDonalds'
+          src={restaurant.imageLink}
+          alt={restaurant.name}
           fill={true}
-          // blurDataURL={placeholderImg}
           style={{ objectFit: 'cover' }}
-          className='rounded-b-lg md:rounded-r-lg md:rounded-bl-none'
+          className="rounded-b-lg md:rounded-r-lg md:rounded-bl-none"
         />
       </div>
     </Card>
+
   );
 }
