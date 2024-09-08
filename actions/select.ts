bd9ac3d266e2722b88738354822b1f4feda0987a
Abhinav Kumar <abhinav.webdj@gmail.com>
2024-09-08 16:39:28 +0530

    removed turso shit

--- actions/select.ts
+++ actions/select.ts
@@ -1,7 +1,10 @@
 import { db } from "@/db/drizzle";
 import { restaurants, SelectRestaurant } from '@/db/schema';
+import { revalidatePath } from "next/cache";
 
 export const getAllRestaurants = async (): Promise<SelectRestaurant[]>=> {
-  const data = await db.select().from(restaurants);
-  return data;
+    const data = await db.select().from(restaurants);
+    revalidatePath('/test');
+    return data;
+    
 }
