833012cc57c67aa72c3a0978db867d749e9500b3
Abhinav Kumar <abhinav.webdj@gmail.com>
2024-10-06 12:12:56 +0530

    removed unecesay stuff from player

--- components/mycomponents/VideoPlayer.tsx
+++ components/mycomponents/VideoPlayer.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState, useEffect } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
-import { ArrowLeft, Globe, Subtitles } from 'lucide-react';
+
 
 interface VideoPlayerProps {
   mediaType: 'movie' | 'tv';
@@ -11,30 +11,20 @@ interface VideoPlayerProps {
 
 const VideoPlayer: React.FC<VideoPlayerProps> = ({ mediaType, id }) => {
   const [embedUrl, setEmbedUrl] = useState<string | null>(null);
-  const [language, setLanguage] = useState('en');
-  const [subtitles, setSubtitles] = useState('');
   const router = useRouter();
   const searchParams = useSearchParams();
   const searchQuery = searchParams?.get('q') || '';
-
-  useEffect(() => {
-    updateEmbedUrl();
-  }, [mediaType, id, language, subtitles]);
 
   const updateEmbedUrl = () => {
     let url = `https://vidsrc.xyz/embed/${mediaType}?tmdb=${id}`;
     if (mediaType === 'tv') {
       url += '&season=1&episode=1';
     }
-    if (language !== 'en') {
-      url += `&ds_lang=${language}`;
-    }
-    if (subtitles) {
-      url += `&sub_url=${encodeURIComponent(subtitles)}`;
-    }
     setEmbedUrl(url);
   };
-
+  useEffect(() => {
+    updateEmbedUrl();
+  }, [mediaType, id]);
   const handleBack = () => {
     const selectedMediaId = searchParams?.get('selectedMediaId') || '';
     const selectedMediaType = searchParams?.get('selectedMediaType') || '';
@@ -47,32 +37,7 @@ const VideoPlayer: React.FC<VideoPlayerProps> = ({ mediaType, id }) => {
   };
 
   return (
-    <div className='flex flex-col h-screen bg-black'>
-      <div className='absolute top-4 left-4 z-10 flex space-x-2'>
-        <button
-          onClick={handleBack}
-          className='text-white bg-gray-800 bg-opacity-50 hover:bg-opacity-75 p-2 rounded-full transition-colors focus:outline-none'
-          aria-label='Go back'
-        >
-          <ArrowLeft size={24} />
-        </button>
-        <button
-          onClick={() => setLanguage(language === 'en' ? 'de' : 'en')}
-          className='text-white bg-gray-800 bg-opacity-50 hover:bg-opacity-75 p-2 rounded-full transition-colors focus:outline-none'
-          aria-label='Change language'
-        >
-          <Globe size={24} />
-        </button>
-        <button
-          onClick={() =>
-            setSubtitles(subtitles ? '' : 'https://vidsrc.me/sample.srt')
-          }
-          className='text-white bg-gray-800 bg-opacity-50 hover:bg-opacity-75 p-2 rounded-full transition-colors focus:outline-none'
-          aria-label='Toggle subtitles'
-        >
-          <Subtitles size={24} />
-        </button>
-      </div>
+    <div className='flex flex-col h-screen  '>
       <div className='flex-grow relative'>
         {embedUrl ? (
           <iframe
