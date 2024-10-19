216e3b80fb41de7fbb552fd46449b62bc6ece428
Abhinav Kumar <abhinav.webdj@gmail.com>
2024-10-19 13:20:24 +0530

    made better

--- app/watch/[mediaType]/[id]/page.tsx
+++ app/watch/[mediaType]/[id]/page.tsx
@@ -80,7 +80,7 @@ export default function WatchPage() {
       </div>
 
       {mediaType === 'tv' && (!isLastEpisodeOfSeason || !isLastSeason) && (
-        <div className='bg-white dark:bg-gray-900 p-10 flex justify-center'>
+        <div className=' p-10 flex justify-center'>
           <button
             onClick={handleNext}
             className='inline-flex items-center justify-center px-10 py-4 text-base font-medium text-white transition-all duration-300 ease-in-out bg-blue-600 rounded-full hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2'
